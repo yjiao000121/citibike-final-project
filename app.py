7283a29d4a908506925af0eb074eeaecd451ebb8
yjiao000121 <yj2916@columbia.edu>
2026-04-20 00:34:22 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="Citi Bike Project", layout="wide")
+
+st.title("Mapping Urban Mobility Through Citi Bike")
+st.write(
+    """
+    This project explores Citi Bike mobility patterns in the Jersey City–Hoboken area
+    using trip data from January to March 2025.
+
+    Use the sidebar to navigate through the different sections of the project.
+    """
+)
